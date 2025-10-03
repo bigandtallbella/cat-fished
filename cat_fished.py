451a429eb116a47866839b4c0c8f78b981cd4c3f
bigandtallbella <b3llatwong@gmail.com>
2025-10-02 21:33:02 -0700

    set up again

--- cat_fished.py
+++ cat_fished.py
@@ -1,1 +1,28 @@
+import pygame
 
+pygame.init()
+
+screen_dim = 1080
+screen = pygame.display.set_mode((screen_dim, screen_dim))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+
+    keyDown = pygame.key.get_pressed()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            exit()
+        if keyDown[pygame.K_ESCAPE]:
+            running = False
+            pygame.quit()
+            print("Hit any key to quit")
+            exit()
+    screen.fill("white")
+
+    pygame.display.update()
+
+    clock.tick(60)
