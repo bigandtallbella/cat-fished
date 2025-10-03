4ba77c5275b352ab5ae597804a13b32c7e815502
bigandtallbella <b3llatwong@gmail.com>
2025-10-02 22:59:53 -0700

    I DID IT!!!!!!!
    chat it took me... an inordinate amount of time but i finally figured out how to put the cat onto the screen
    
    *note to check parameter inputs

--- cat_fished.py
+++ cat_fished.py
@@ -3,9 +3,25 @@ import pygame
 pygame.init()
 
 screen_dim = 1080
-screen = pygame.display.set_mode((screen_dim, screen_dim))
+screen = pygame.display.set_mode((screen_dim, screen_dim)) # set display screen
 clock = pygame.time.Clock()
 running = True
+
+bg = pygame.image.load('fishing_bg.png')
+bg = bg.convert_alpha() # better read on transparency
+
+cat = pygame.image.load('default_kitty.png')
+cat = cat.convert_alpha() # better read on transparency
+
+def background_img(image): # function = def
+    size = pygame.transform.scale(bg, (screen_dim, screen_dim))
+    screen.blit(size, (0,0)) 
+def kitty_spawn(image):
+    size = pygame.transform.scale(cat, (screen_dim, screen_dim))
+    screen.blit(size, (0,0)) # render sprite at size at coordinates (0,0)
+
+        
+# class Player():
 
 while running:
 
@@ -21,8 +37,9 @@ while running:
             pygame.quit()
             print("Hit any key to quit")
             exit()
-    screen.fill("white")
 
-    pygame.display.update()
-
+    background_img(bg) 
+    kitty_spawn(cat)
+    pygame.display.update()# do this after function call to render sprite
+    
     clock.tick(60)
